81961e3be6aeeace59653f1655756ac86df85fac
JohnComputers <johncomputers2024@gmail.com>
2026-05-14 07:15:33 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,11 +11,13 @@
      → "SDK setup and configuration" → "Config"
 ============================================================ */
 
+
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDPyf7Wtb5rNwaSKFGfzHe3pCiwklnq1qs",
   authDomain: "early-bird-attendeance.firebaseapp.com",
   projectId: "early-bird-attendeance",
   storageBucket: "early-bird-attendeance.firebasestorage.app",
   messagingSenderId: "1036831010751",
-  appId: "1:1036831010751:web:efa50c32ed1a0219ea7b0a"
+  appId: "1:1036831010751:web:efa50c32ed1a0219ea7b0a",
+  measurementId: "G-3G1GETZ9J0"
 };
